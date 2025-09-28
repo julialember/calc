c979f1f1028bc1271f446e9a53793d7b7142964d
stanislav <pizzabosew@mail.ru>
2025-09-28 23:22:50 +0300

    calc 2.1

--- main.c
+++ main.c
@@ -1,34 +1,41 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#define MAXBUFFER 100
+int buffer[MAXBUFFER];
+size_t buffer_size = 0;
+
+void push(int part) {
+    if (buffer_size <= MAXBUFFER) buffer[buffer_size++] = part;
+    else printf("too much symbols in your expr\n");
+}
+
+int pop() {
+    return buffer_size > 0 ? buffer[--buffer_size] : -1;
+}
+
+void convert_to_post(char* argv[]) {
+    
+}
 
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        printf("at least 3 args");
-        return 1;
-    };
-    int first = atoi(argv[1]);
-    int second = atoi(argv[3]);
-    switch (argv[2][0]) {
-        case '-':
-            printf("%d\n", first - second);
-            break;
-        case '+':
-            printf("%d\n", first + second);
-            break;
-        case '/':
-            if (second == 0) {
-                printf("you can't div to 0\n");
-                return 1;
+    for (int i = 1; i < argc; i++) {
+        if (isdigit(argv[i][0]))
+            push(atoi(argv[i]));
+        else switch (argv[i][0]) {
+            case '-':  {
+                int second = pop(); 
+                printf("%d\n", pop() - second); 
+                break;
             };
-            printf("%g\n", (float) first/second);
-            break;
-        case 'x':
-            printf("%d\n", first * second);
-            break;
-        default:
-            printf("unknown operation\n");
-            return 1;
+            case '+': 
+                printf("%d\n", pop() + pop());
+                break;
+            default: 
+                printf("unknown operation\n");
+                return 1;
+        }
     }
     return 0;
 }
