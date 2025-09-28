15324ec12aa5eb2a60dbee3d087c0eefb1c2344f
stanislav <pizzabosew@mail.ru>
2025-09-28 23:46:46 +0300

    im going to sleep

---
+++ README.md
@@ -0,0 +1,20 @@
+DOCUMENTATION
+operation hierarchy
+
+()
+/ * ^  
+- + 
+
+5 + 3 * 2 = 3 2 * 5 +
+(5 + 3) * 2 = 5 3 + 2 *
+8 - 4 / 2 = 8 4 2 / -
+(8 - 4) / 2 = 8 4 - 2 /
+2 * 3 / 4 * 5 = 2 3 * 4 / 5 * 
+2^(3 + 1) = 2 3 1 + ^ 
+10 - 2 * 3 + 1 = 10 2 3 * - 1 + 
+(1 + 2) * (3 + 4) = 1 2 + 3 4 + *
+
+three levels of hierarchy. 
+let solve the example 5 * 4 + 2 
+we try to find (), next we try to find / or * 
+find it! push into array the expression 5 
--- main.c
+++ main.c
@@ -1,41 +1,12 @@
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
 #define MAXBUFFER 100
-int buffer[MAXBUFFER];
-size_t buffer_size = 0;
+char buffer[MAXBUFFER];
 
-void push(int part) {
-    if (buffer_size <= MAXBUFFER) buffer[buffer_size++] = part;
-    else printf("too much symbols in your expr\n");
-}
-
-int pop() {
-    return buffer_size > 0 ? buffer[--buffer_size] : -1;
-}
-
-void convert_to_post(char* argv[]) {
-    
+void convert_to_post(int argc, char** argv[]) {  
+    for (int i = 1; i < argc; i++) {
+        
+    }
 }
 
 int main(int argc, char *argv[]) {
-    for (int i = 1; i < argc; i++) {
-        if (isdigit(argv[i][0]))
-            push(atoi(argv[i]));
-        else switch (argv[i][0]) {
-            case '-':  {
-                int second = pop(); 
-                printf("%d\n", pop() - second); 
-                break;
-            };
-            case '+': 
-                printf("%d\n", pop() + pop());
-                break;
-            default: 
-                printf("unknown operation\n");
-                return 1;
-        }
-    }
     return 0;
 }
