dc4c8642296fd46b3005da2b998a9dda1f8dfb8c
stanislav <pizzabosew@mail.ru>
2025-09-29 21:47:56 +0300

    hello

--- main.c
+++ main.c
@@ -3,45 +3,40 @@
 #include <stdlib.h>
 #define MAXBUFFER 20
 typedef struct {
-    int num;
+    char *text;
     int mode;
 } element;
 
 element stack[MAXBUFFER];
 int pos = 0;
-int ex[MAXBUFFER];
+char* ex[MAXBUFFER];
 int o_pos = 0;
 
 void push(element);
-int set_pri(char);
+int set_pri(char*);
 
 int main(int argc, char* argv[]) {
-    printf("args: ");
-    for (int i = 1; i < argc; i++) printf("%d: %s\n", i, argv[i]);
     element now_el;
     for (int i = 1; i < argc; i++) {
         if (isdigit(argv[i][0])) {
             now_el.mode = 1;
-            now_el.num = atoi(argv[i]);
+            now_el.text = argv[i];
             push(now_el);
         } else if (o_pos <= MAXBUFFER) {
-            for (int hier = set_pri(argv[i][0]); hier <= set_pri(ex[o_pos]) && o_pos > 0; o_pos--) {
-                now_el.mode = 0;
-                now_el.num = ex[o_pos];
+            for (int hier = set_pri(argv[i]); o_pos > 0 && hier <= set_pri(ex[o_pos-1]);) {
+                now_el.mode =0;
+                now_el.text = ex[--o_pos];
                 push(now_el);
             }
-            ex[o_pos++] = argv[i][0];
-            printf("oper: %c\n", (char)ex[o_pos-1]);
+            ex[o_pos++] = argv[i];
         }
-    }
-    while (o_pos >= 0) {
-        now_el.mode = 0;
-        now_el.num = ex[o_pos--];
-        push(now_el);
-    }
-    for (int i = 0; i < pos; i++) {
-        if (stack[i].mode) printf("%d ", stack[i].num); else printf("%c ", (char) stack[i].num);
     };
+    now_el.mode = 0;
+    while (o_pos>0) {
+        now_el.text = ex[--o_pos];
+        push(now_el);
+    };
+    for (int i = 0; i < pos; i++) printf("%s ", stack[i].text);
     putchar('\n');
     return 0;
 }
@@ -51,8 +46,8 @@ void push(element el) {
     stack[pos++] = el;
 }
 
-int set_pri(char op) {
-    switch (op) {
+int set_pri(char* op) {
+    switch (op[0]) {
         case '-': case '+':
             return 1;
         case '/': case 'x':
